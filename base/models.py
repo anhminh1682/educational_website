a3f74b7011517442f5bdc3df24fc7ac3019c023f
dvnam1401 <vannamdang2003@gmail.com>
2023-09-24 02:17:24 +0700

    id

--- base/models.py
+++ base/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+# # Create your models here.
+# class User(models.Model):
+#     username = models.CharField(max_length=100, unique=True)
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.username
+#     def __str__(self):
+#         return self.username
 
 class Post(models.Model):
     title = models.CharField(max_length=200,null=True)
